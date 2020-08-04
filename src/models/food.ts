0042165458491174aff01f13c65ffc3d879f6a91
King Elisha <ek.chibueze@gmail.com>
2020-08-04 21:37:24 +0100

    feat: updated food model

--- src/models/food.ts
+++ src/models/food.ts
@@ -29,86 +29,89 @@ interface IFood extends Document {
   saturatedFats: number;
 }
 
-const FoodSchema = new Schema({
-  foodCode: {
-    type: Number,
-    unique: true,
+const FoodSchema = new Schema(
+  {
+    foodCode: {
+      type: Number,
+      unique: true,
+    },
+    displayName: {
+      type: String,
+    },
+    portionDefault: {
+      type: Number,
+    },
+    portionAmount: {
+      type: Number,
+    },
+    portionDisplayName: {
+      type: String,
+    },
+    factor: {
+      type: Number,
+    },
+    incrementF: {
+      type: Number,
+    },
+    multiplier: {
+      type: Number,
+    },
+    grains: {
+      type: Number,
+    },
+    wholeGrains: {
+      type: Number,
+    },
+    vegetables: {
+      type: Number,
+    },
+    orangeVegetables: {
+      type: Number,
+    },
+    drkgreenVegetables: {
+      type: Number,
+    },
+    starchyvegetables: {
+      type: Number,
+    },
+    otherVegetables: {
+      type: Number,
+    },
+    fruits: {
+      type: Number,
+    },
+    milk: {
+      type: Number,
+    },
+    meats: {
+      type: Number,
+    },
+    soy: {
+      type: Number,
+    },
+    drybeansPeas: {
+      type: Number,
+    },
+    oils: {
+      type: Number,
+    },
+    solidFats: {
+      type: Number,
+    },
+    addedSugars: {
+      type: Number,
+    },
+    alcohol: {
+      type: Number,
+    },
+    calories: {
+      type: Number,
+    },
+    saturatedFats: {
+      type: Number,
+    },
   },
-  displayName: {
-    type: String,
-  },
-  portionDefault: {
-    type: Number,
-  },
-  portionAmount: {
-    type: Number,
-  },
-  portionDisplayName: {
-    type: String,
-  },
-  factor: {
-    type: Number,
-  },
-  incrementF: {
-    type: Number,
-  },
-  multiplier: {
-    type: Number,
-  },
-  grains: {
-    type: Number,
-  },
-  wholeGrains: {
-    type: Number,
-  },
-  vegetables: {
-    type: Number,
-  },
-  orangeVegetables: {
-    type: Number,
-  },
-  drkgreenVegetables: {
-    type: Number,
-  },
-  starchyvegetables: {
-    type: Number,
-  },
-  otherVegetables: {
-    type: Number,
-  },
-  fruits: {
-    type: Number,
-  },
-  milk: {
-    type: Number,
-  },
-  meats: {
-    type: Number,
-  },
-  soy: {
-    type: Number,
-  },
-  drybeansPeas: {
-    type: Number,
-  },
-  oils: {
-    type: Number,
-  },
-  solidFats: {
-    type: Number,
-  },
-  addedSugars: {
-    type: Number,
-  },
-  alcohol: {
-    type: Number,
-  },
-  calories: {
-    type: Number,
-  },
-  saturatedFats: {
-    type: Number,
-  },
-});
+  { timestamps: true }
+);
 
 export const Food = model<IFood>("Food", FoodSchema);
